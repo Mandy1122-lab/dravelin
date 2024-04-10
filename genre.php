2efd9cc084deafb7b14306be894ae4dab45546d8
wennychang <131533115+wennychang@users.noreply.github.com>
2024-04-11 00:02:47 +0800

    Add files via upload

---
+++ genre.php
@@ -0,0 +1,269 @@
+<?php
+include_once "{$_SEVER['DOCUMENT_ROOT']}/lib/lib_mysql.php" ;
+$conn = sql_open(); 
+$sql = "SELECT * FROM genre";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>分類</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/plyr.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <!--font-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
+    <script src="https://kit.fontawesome.com/937e93c93c.js" crossorigin="anonymous"></script>
+    
+
+    <style>
+        
+        .heading_logo,.heading_logo a{
+            color: white;
+            font-weight: bold;
+            font-size: 30px;
+            margin-top: 7px;
+        }
+        .section_title{
+            margin-bottom: 30px;
+            color: #000;
+            font-weight: 600;
+            line-height: 21px;
+            text-transform: uppercase;
+            padding-left: 20px;
+            position: relative;
+        }
+        .section_title,.section_title h4{
+            margin-bottom: 30px;
+            color: #000;
+            font-weight: bolder;
+            line-height: 21px;
+            text-transform: uppercase;
+            padding-left: 15px;
+            position: relative;
+            
+        }
+        .section_title{
+            border-bottom: 1px solid;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+        td, th {
+            border: 1px solid #B1B1B1;
+            text-align: left;
+            padding: 8px;
+            text-align: center;
+            }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+        tr th,tr:nth-child(odd){
+            background-color: #f5f5f5;
+        }
+        .my-btn{
+            background-color: #F5F4F0;
+            border-color: #1d50a1;
+            color:#1d50a1;
+            font-size: 18;
+            font-weight: bold;
+            margin-left: 84%;
+            width: 120px;
+
+        }
+        .my-btn:hover{
+            background-color: #1d50a1;
+            border-color: #1d50a1;
+            color: #f5f4f0;
+        }
+        .my-btn:active{
+            background-color: #1d50a1 !important;
+            border-color: #1d50a1 !important;
+            color: #f5f4f0 !important;
+        }
+        .wrapper{
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            margin-bottom: 15px;
+        }
+        .search{
+            max-width: 150px;
+            background-color: #D9D9D9;
+            border-color: #1d50a1;
+            font-family: Inter;
+        }
+    </style>
+</head>
+
+<body class="font_set">
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row"> 
+                <div class="col-lg-2">
+                    <div class="heading_logo">
+                        <a href="./index.html">
+
+                            Dravelin
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li><a href="#">劇集<span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">C</a></li>
+                                        <li><a href="#">A</a></li>
+                                        <li><a href="#">A</a></li>
+                                        <li><a href="#">B</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">電影<span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">C</a></li>
+                                        <li><a href="#">A</a></li>
+                                        <li><a href="#">A</a></li>
+                                        <li><a href="#">a</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">拍攝景點<span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">台灣</a></li>
+                                        <li><a href="#">韓國</a></li>
+                                        <li><a href="#">日本</a></li>
+                                        <li><a href="#">泰國</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">活動專區</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="header__right">
+                        <a href="#" class="search-switch"><i class="fa-solid fa-magnifying-glass fa-lg" style="color: #ffffff;"></i></a>
+                        <!-- <a href="./login.html"><span class="icon_profile"></span></a> -->
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </header>
+    <!-- Header End -->
+</head>
+
+<section class="product-page spad">
+    <div class="container">
+        <div>
+            <div class="section_title">
+                <h4>分類列表</h4>
+            </div>
+
+            <div class="wrapper">
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2 search" type="search" placeholder="搜尋分類" aria-label="Search">
+                </form>
+                <button onclick="window.location.href='spot-add.php'" type="button" class="btn btn-outline-primary my-btn">＋&nbsp;新增分類</button>
+            </div>
+                
+                 <?php
+                 $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table>";
+                        echo "<tr><th>#</th><th>分類</th><th>操作</th></tr>";
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>{$row['g_id']}</td>";
+                            echo "<td align='center'>{$row['g_name']}</td>";
+                            echo '<td align="center"><a href="?g_id=' . $row['g_id'] . '"><i class="fa-solid fa-pen" style="color: #1d50a1;"></i></i></a>&nbsp;&nbsp;&nbsp;<a href="?g_id=' . $row['g_id'] . '"><i class="fa-solid fa-trash" style="color: #de2626;"></i></a></td>';
+                            echo "</tr>";
+                        }
+                
+                        echo "</table></center>";
+                        echo "</div>";
+                    
+                    }
+            
+                    mysqli_close($link);
+                ?> 
+
+        </div>
+        
+    </div>
+</section>
+
+
+
+<!-- Footer Section Begin -->
+<footer class="footer">
+    <div class="page-up">
+        <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer__logo">
+                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="footer__nav">
+                    <ul>
+                        <li class="active"><a href="./index.html">Homepage</a></li>
+                        <li><a href="./categories.html">Categories</a></li>
+                        <li><a href="./blog.html">Our Blog</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+
+<!-- Js Plugins -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/player.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/mixitup.min.js"></script>
+<script src="js/jquery.slicknav.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
