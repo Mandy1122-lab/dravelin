4c750cddf8327fbb52cf078f6114e27b2907e22d
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-04-27 21:40:40 +0800

    Update spot.php

--- spot.php
+++ spot.php
@@ -130,7 +130,7 @@
             <div class="col-lg-8 col-md-8 col-sm-6 section_title">
                 <h4 class="c-title">以國家分類</h4>
             </div>
-            <div class="country" style="padding-left:30px !important;grid-gap:40px">
+            <div class="country" style="padding-left:30px !important;grid-gap:40px !important">
             <?php  
                 include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
                 $conn = sql_open();
