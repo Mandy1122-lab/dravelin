4d79850280556d0ac38cfffe83a1dbf12167fbd1
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-01 00:22:39 +0800

    Update spot-del.php

--- spot-del.php
+++ spot-del.php
@@ -1,20 +1,21 @@
 <?php 
     if (isset($_GET["s_id"])) {
-        $link = @mysqli_connect('localhost', 'root', 'root', 'dravelin') or die("無法開啟資料庫連接");
-
-        $sid = mysqli_real_escape_string($link, $_GET["s_id"]);
+        include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+        $conn = sql_open();
+        
+        $sid = $_GET["s_id"];
 
 
         $sql = "DELETE FROM spot WHERE s_id = '$sid'";
 
-        if (mysqli_query($link, $sql)) {
+        if (mysqli_query($conn, $sql)) {
             echo "刪除成功";
         } else {
-            echo "錯誤： " . $sql . "<br>" . mysqli_error($link);
+            echo "錯誤： " . $sql . "<br>" . mysqli_error($conn);
         }
 
-        mysqli_close($link);
-        header("Location: spot-manage.php");
+        mysqli_close($conn);
+        header("Location:spot-manage.php");
         exit();
     }
 ?>
