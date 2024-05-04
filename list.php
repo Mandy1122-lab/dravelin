d0da11f00927b73d8d78d58be295de9c3cff058c
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-04 20:24:57 +0800

    Update list.php

--- list.php
+++ list.php
@@ -75,66 +75,23 @@
     </div>
 
     <!-- Header Section Begin -->
-    <header class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-2">
-                    <div class="heading_logo">
-                        <a href="./index.html">
-                            <!-- <img src="img/logo.png" alt=""> -->
-                            Dravelin
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <div class="header__nav">
-                        <nav class="header__menu mobile-menu">
-                            <ul>
-                                <li><a href="#">劇集<span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="#">Categories</a></li>
-                                        <li><a href="#">Anime Details</a></li>
-                                        <li><a href="#">Anime Watching</a></li>
-                                        <li><a href="#">Blog Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">電影<span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="#">Categories</a></li>
-                                        <li><a href="#">Anime Details</a></li>
-                                        <li><a href="#">Anime Watching</a></li>
-                                        <li><a href="#">Blog Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">拍攝景點<span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="#">Categories</a></li>
-                                        <li><a href="#">Anime Details</a></li>
-                                        <li><a href="#">Anime Watching</a></li>
-                                        <li><a href="#">Blog Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">活動專區</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-lg-2">
-                    <div class="header__right">
-                        <a href="#" class="search-switch"><i class="fa-solid fa-magnifying-glass fa-lg" style="color: #ffffff;"></i></a>
-                        <!-- <a href="./login.html"><span class="icon_profile"></span></a> -->
-                    </div>
-                </div>
-            </div>
-            <div id="mobile-menu-wrap"></div>
-        </div>
-    </header>
+    <?php 
+        include 'header.html';
+        echo "<br>";
+        echo "<a href='spot.php' style='margin-left:150px'><i class='fa-solid fa-arrow-left fa-xl' style='color: #1d50a1;'></i></a>";
+
+    ?> 
     <!-- Header End -->
 
-
+    
+    
     <!-- Product Section Begin -->
     <section class="product-page spad">
+    
+
         <div class="container">
+        
+
         <?php 
             include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
             $conn = sql_open();
@@ -156,9 +113,9 @@
             if ($result = mysqli_query($conn, $sql)) {
                 if ($row = mysqli_fetch_assoc($result)) {
                     $sc_name = $row['sc_name'];
-            
-                    echo "<div class='col-lg-8 col-md-8 col-sm-6 section_title' >";
-                    echo "<a href='spot.php' style='align-self: center;'><i class='fa-solid fa-angles-left fa-xl' style='color: #1d50a1;'>&nbsp;&nbsp;$sc_name</i></a>";
+                    echo "<div class='col-lg-8 col-md-8 col-sm-6 section_title' style='margin-top:-30px'>";
+                    echo "<p class='s-title' style='align-self: center;color: #1d50a1;'><b>$sc_name</b></p>";
+                    
                     echo "</div>";
             
                     do {
