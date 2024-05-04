99e3b9b08d5568d55db69a0af0aca04142be8d9b
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-04 23:06:28 +0800

    Update spot-edit.php

--- spot-edit.php
+++ spot-edit.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="Anime, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>編輯-景點介紹</title>
+    <title>編輯、刪除 - 景點合輯</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -23,13 +23,136 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="stylesheet" href="dec.css" type="text/css">
     <!--font-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
     <script src="https://kit.fontawesome.com/937e93c93c.js" crossorigin="anonymous"></script>
     
 
-    
+    <style>
+        
+        .heading_logo,.heading_logo a{
+            color: white;
+            font-weight: bold;
+            font-size: 30px;
+            margin-top: 7px;
+        }
+        .section_title{
+            margin-bottom: 30px;
+            color: #000;
+            font-weight: 600;
+            line-height: 21px;
+            text-transform: uppercase;
+            padding-left: 20px;
+            position: relative;
+        }
+        .section_title,.section_title h4{
+            margin-bottom: 30px;
+            color: #000;
+            font-weight: bolder;
+            line-height: 21px;
+            text-transform: uppercase;
+            padding-left: 15px;
+            position: relative;
+            
+        }
+        .section_title{
+            border-bottom: 1px solid;
+        }
+        table {
+            border-collapse: collapse;
+            width: 90%;
+            /* border-radius: 50% !important;  */
+
+            }
+
+        td, th {
+            border: 1px solid #1d50a1;
+            padding: 8px;
+            text-align: center;
+            background-color: #f5f4f0;
+            
+            }
+        
+        
+        
+        .my-btn{
+            background-color: #F5F4F0;
+            border-color: #1d50a1;
+            color:#1d50a1;
+            font-size: 18;
+            font-weight: bold;
+            margin-left: 84%;
+            width: 120px;
+
+        }
+        .my-btn:hover{
+            background-color: #1d50a1;
+            border-color: #1d50a1;
+            color: #f5f4f0;
+        }
+        .my-btn:active{
+            background-color: #1d50a1 !important;
+            border-color: #1d50a1 !important;
+            color: #f5f4f0 !important;
+        }
+        .search{
+            max-width: 220px;
+            background-color: #D9D9D9;
+            border-color: #1d50a1;
+            font-family: Inter;
+
+            margin-top: 25px;
+        }
+
+    .cancel{
+            background-color: #abbad5;
+            border-color: #abbad5;
+            color:black !important;
+            font-weight: bold;
+            margin-left: 67%;
+            margin-top: 60px;
+        }
+        .cancel:hover,
+        .cancel:active{
+            background-color: #F5F4F0!important;
+            border-color: #abbad5!important;
+            color:#abbad5 !important; 
+        }
+
+        .save{
+            background-color: #FED566;
+            border-color: #FED566;
+            color:black !important;
+            font-weight: bold;
+            margin-left: 10px;
+            margin-top: 60px;
+        }
+        .save:hover,
+        .save:active{
+            background-color: #F5F4F0!important;
+            border-color: #FED566!important;
+            color:#FED566 !important; 
+        }
+        .input{
+            border-color: #1d50a1;
+            background-color: #F5F4F0;
+            width:500px !important;
+
+        }
+
+        .delete{
+            background-color: #f5f4f0;
+            border-color: #EA5A47;
+            color:#EA5A47 !important;
+            font-weight: bold;
+        }
+        .delete:hover,
+        .delete:active{
+            background-color: #EA5A47!important;
+            border-color: #EA5A47!important;
+            color:#f5f4f0 !important; 
+        }
+    </style>
 </head>
 
 <body class="font_set">
@@ -42,27 +165,24 @@
     <?php 
     include 'header-mng.html';
     echo "<br>";
-    echo "<a href='spot-manage.php' style='margin-left:150px'><i class='fa-solid fa-arrow-left fa-xl' style='color: #1d50a1;'></i></a>";
+    echo "<a href='spot-complication.php' style='margin-left:150px'><i class='fa-solid fa-arrow-left fa-xl' style='color: #1d50a1;'></i></a>";
 
     ?>
     <!-- Header End -->
-    <!--原有資料-->
+    <!--帶入資料-->
     <?php 
-    if (isset($_GET["s_id"])){
+    if (isset($_GET["sc_id"])){
         include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
         $conn = sql_open();
-        $s_id = $_GET["s_id"];
+        $sc_id = $_GET["sc_id"];
 
-        $sql = "SELECT * FROM spot WHERE s_id ='$s_id'";
+        $sql = "SELECT * FROM scomplication WHERE sc_id ='$sc_id'";
         $result = mysqli_query($conn,$sql);
         if ($row = mysqli_fetch_assoc($result)){
         $existingData = [
-            's_name' => $row['s_name'],
-            's_add' => $row['s_add'],
-            's_info' => $row['s_info'],
-            's_intro' => $row['s_intro'],
-            's_photo' => $row['s_photo'],
-            's_pic' => $row['s_pic'],
+            'sc_name' => $row['sc_name'],
+            'sc_pic' => $row['sc_pic'],
+            'sc_intro' => $row['sc_intro']
 
         ];
         }else{
@@ -72,27 +192,23 @@
         mysqli_close($conn);
     }
     ?> 
-
     <!--修改PHP-->
     <?php
     if (isset($_POST["Update"])) {
         include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
         $conn = sql_open();
-        $s_id = $_POST["s_id"];
-        $s_name = $_POST["s_name"];
-        $s_add = $_POST["s_add"];
-        $s_intro = $_POST["s_intro"];
-        $s_info = $_POST["s_info"];
-        $s_photo = $_POST["s_photo"];
-        $s_pic = $_POST["s_pic"];
+        $sc_id = $_POST["sc_id"];
+        $sc_name = $_POST["sc_name"];
+        $sc_pic = $_POST["sc_pic"];
+        $sc_intro = $_POST["sc_intro"];
 
-        $sql = "UPDATE spot SET s_name = '$s_name', s_add = '$s_add', s_intro = '$s_intro', s_info = '$s_info', s_photo = '$s_photo', s_pic = '$s_pic' WHERE s_id='$s_id' ";
+        $sql = "UPDATE scomplication SET sc_name = '$sc_name', s_pic = '$s_pic' , sc_intro = '$sc_intro' WHERE sc_id='$sc_id' ";
 
         if (mysqli_query($conn, $sql)) {
             mysqli_close($conn);
-            echo '<script>window.location.href = "spot-manage.php";</script>';
+            echo '<script>window.location.href = "spot-complication.php";</script>';
             exit();
-        } else { 
+        } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
 
@@ -100,69 +216,54 @@
     }
     ?>
 
+
+
     <!-- Product Section Begin -->
-    <section class="product-page spad">
+    <section class="product-page spad" style="margin-top: -30px;">
         <div class="container">
             <div>
-                <div class="section_title line" style='margin-top:-30px'>
-                    <h4 class="s-edit-del">編輯 - 景點介紹</h4>
-                    <button type="button" class="btn btn-outline-primary delete" onclick="return confirmAction(<?php echo $row['s_id']; ?>)">刪除景點</button>
+                <!--標題-->
+                <div class="section_title">
+                    <nobr>
+                    <h4 style="display: inline-block !important;">編輯、刪除 - 景點合輯</h4>
+                    <button type="button" class="btn btn-outline-primary delete" style="margin-left:64%;display: inline-block !important;" onclick="return confirmAction(<?php echo $row['sc_id']; ?>)">刪除合輯</button>
                     <script>
-                        function confirmAction(s_id) {
+                        function confirmAction(sc_id) {
                             
                             var result = confirm("是否確認刪除？");
                             if (result) {
-                                window.location.href = 'spot-del.php?s_id=' + s_id;
+                                window.location.href = 'spotcoll-del.php?sc_id=' + sc_id;
                             }
                             return false;
                         }
                         </script>
-                </div>
-                <form action="" method="post">
-                    <input type="hidden" name="s_id" value="<?php echo $s_id; ?>">
-                    <div class="row">
-                    <div class="col">
-                        <label for="inputEmail4">景點名稱</label>
-                        <input type="text" name="s_name" class="form-control" placeholder="請輸入名稱" value="<?php echo $existingData['s_name'];?>">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">地址</label>
-                        <input type="text" name="s_add" class="form-control" placeholder="請輸入地址" value="<?php echo $existingData['s_add'];?>">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">景點地圖（Google Map）</label>
-                        <input type="text" class="form-control" placeholder="請輸入網址">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">景點資訊</label>
-                        <input type="text" name="s_info" class="form-control" placeholder="請輸入營業時間、電話等資訊" value="<?php echo $existingData['s_info'];?>">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">在此取景作品</label><br>
-                        <select class="form-control select" id="exampleFormControlSelect1" >
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            </select>
-                        
-                    </div>
-                    <div class="col">
-                        <label for="inputEmail4">景點簡介</label>
-                        <textarea class="form-control" name="s_intro" placeholder="請輸入內容" rows="5"><?php echo $existingData['s_intro']; ?></textarea>
-
-
-                        <label for="inputEmail4" style="margin-top: 25px;">相關劇照</label>
-                        <input type="text" name="s_photo" class="form-control" placeholder="請輸入位置" value="<?php echo $existingData['s_photo'];?>">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">景點封面圖</label>
-                        <input type="text" name="s_pic" class="form-control" placeholder="請輸入位置" value="<?php echo $existingData['s_pic'];?>">
-                        <nobr>
-                        <button type="reset" class="btn btn-outline-primary cancel" onclick="window.location.href='spot-manage.php'">取消</button>
-                        <button type="submit" name="Update" onclick="return confirmaction()" class="btn btn-outline-primary save">儲存</button>
-                        </nobr>
-
-                    </div> 
                     
+                    </nobr>
+                </div>
+                <!--切割版面-->
+                
+                <form action="spotcoll-edit.php" method="post">
+                <input type="hidden" name="sc_id" value="<?php echo $sc_id; ?>">
+                <div style="display:grid;grid-template-columns:1fr 1fr">
+                    <!--左半（搜尋結果）-->
+                    <div>
+                    <label for="inputEmail4">合輯名稱</label>
+                    <input type="text" class="form-control input" placeholder="請輸入名稱" name="sc_name" value="<?php echo $existingData['sc_name'];?>">
+                    <label for="inputEmail4" style="margin-top: 25px;" >合輯封面圖</label>
+                    <input type="text" class="form-control input" placeholder="請輸入位置" name="sc_pic" value="<?php echo $existingData['sc_pic'];?>">
+                    <label for="inputEmail4" style="margin-top: 25px;">合輯簡介</label>
+                    <textarea class="form-control input" placeholder="請輸入內容" rows="5" style="height: 150px;" name="sc_intro" value="<?php echo $existingData['sc_intro'];?>"></textarea>
                     </div>
-                </form>
+                    <!--右半（目前列表）-->
+                    <div style="padding-top:270px">
+                        <nobr>
+                        <button type="reset" onclick="window.location.href='spot-complication.php'" class="btn btn-outline-primary cancel" >取消</button>
+                        
+                        <button type="submit" name="Update" class="btn btn-outline-primary save" >儲存</button>
+                        </nobr>
+                    </div>
+                </div>
+                </form>    
     
                 
             </div>
@@ -224,7 +325,6 @@
 <script src="js/jquery.slicknav.js"></script>
 <script src="js/owl.carousel.min.js"></script>
 <script src="js/main.js"></script>
-
 
 </body>
 
