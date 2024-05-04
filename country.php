ee781ae0416cb2fb99b184ccb2d89ddfe3a8b365
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-04 21:07:41 +0800

    Update country.php

--- country.php
+++ country.php
@@ -75,55 +75,94 @@
     </div>
 
     <!-- Header Section Begin -->
-    <?php include 'header.html'?>
+    <?php 
+    include 'header.html';
+    echo "<br>";
+    echo "<a href='spot.php' style='margin-left:150px'><i class='fa-solid fa-arrow-left fa-xl' style='color: #1d50a1;'></i></a>";
+
+    ?>
     <!-- Header End -->
 
 
     <!-- Product Section Begin -->
     <section class="product-page spad">
         <div class="container">
-        <?php 
-            include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
-            $conn = sql_open();
-            $c_id = $_GET["c_id"];
-            $sql = "SELECT country.c_name, spot.s_name, spot.s_pic, spot.s_info, spot.s_add, spot.s_id
-            FROM cspot 
-            JOIN spot ON cspot.s_id = spot.s_id 
-            JOIN country ON cspot.c_id = country.c_id 
-            WHERE country.c_id = '$c_id'";
-            if ($result = mysqli_query($conn, $sql)) {
-            
-                $row = mysqli_fetch_assoc($result);
-                $c_name = $row['c_name'];
+        <?php
+include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+$conn = sql_open();
+$c_id = $_GET["c_id"];
+$sql = "SELECT 
+            country.c_name, 
+            spot.s_name, 
+            spot.s_pic, 
+            spot.s_info, 
+            spot.s_add, 
+            spot.s_id, 
+            GROUP_CONCAT(DISTINCT drama.d_name ORDER BY drama.d_name SEPARATOR '、') AS drama_names,
+            GROUP_CONCAT(DISTINCT movie.m_name ORDER BY movie.m_name SEPARATOR '、') AS movie_names
+        FROM 
+            cspot 
+        JOIN 
+            spot ON cspot.s_id = spot.s_id 
+        JOIN 
+            country ON cspot.c_id = country.c_id 
+        LEFT JOIN 
+            spotm ON spot.s_id = spotm.s_id
+        LEFT JOIN 
+            movie ON spotm.m_id = movie.m_id
+        LEFT JOIN 
+            spotd ON spot.s_id = spotd.s_id
+        LEFT JOIN 
+            drama ON spotd.d_id = drama.d_id
+        WHERE 
+            country.c_id = '$c_id'
+        GROUP BY 
+            spot.s_id";
 
-                echo "<div class='col-lg-8 col-md-8 col-sm-6 section_title' style='display:grid; grid-template-columns: 1fr 5fr;'>";
-                echo "<a href='spot.php' style='align-self: center;font-size:24px;color: #1d50a1;'><i class='fa-solid fa-arrow-left' style='color: #1d50a1;'></i>&nbsp;&nbsp;$c_name</i></a>";
-                echo "</div>";
-
-
-            while ($row = mysqli_fetch_assoc($result)) { 
-                echo "<div style='display:grid;grid-template-columns:2fr 5fr 1fr;grid-gap:30px;margin:auto auto auto 30px;padding-top:30px;width:100%;border-top:1px solid;'>";
-                echo "<div><img style='width: 250px !important;height:170px;border-radius: 20px;' src='{$row['s_pic']}'></div>";
-                echo "<div>";
-                echo "<p class='s-title'><b>{$row['s_name']}</b></p>";
-                echo "<div class='s-wrap'>";
-                echo "<p class='s-content'><b>地址</b></p>";
-                echo "<p class='s-content'>{$row['s_add']}</p>";
-                echo "<p class='s-content'><b>景點資訊</b></p>";
-                echo "<p class='s-content'>{$row['s_info']}</p>";
-                echo "<p class='s-content'><b>在此取景作品</b></p>";
-                echo "<a href='#'><p class='s-content drama'><b>想見你</b></p></a>";
-                echo "</div>";
-                echo "</div>";
-                echo "<div style='position: relative;'><a style='font-size:20px;position:absolute;bottom:12px' href='spot-info.php?s_id=" . $row['s_id'] . "'><i class='fa-solid fa-circle-info' style='color: #1d50a1;'>&nbsp;&nbsp;詳細資訊</i></a></div>";
-                echo "</div>";
+if ($result = mysqli_query($conn, $sql)) {
+    if ($row = mysqli_fetch_assoc($result)) {
+        $c_name = $row['c_name'];
+        echo "<div class='col-lg-8 col-md-8 col-sm-6 section_title' style='margin-top:-30px'>";
+        echo "<p class='s-title' style='align-self: center;color: #1d50a1;'><b>$c_name</b></p>";
+        echo "</div>";}
+    while ($row = mysqli_fetch_assoc($result)) {
         
+        
+        echo "<div style='display:grid;grid-template-columns:2fr 5fr 1fr;grid-gap:30px;margin:auto auto auto 30px;padding-top:30px;width:100%;border-top:1px solid;'>";
+        echo "<div><img style='width: 250px !important;height:170px;border-radius: 20px;' src='{$row['s_pic']}'></div>";
+        echo "<div>";
+        echo "<p class='s-title'><b>{$row['s_name']}</b></p>";
+        echo "<div class='s-wrap'>";
+        echo "<p class='s-content'><b>地址</b></p>";
+        echo "<p class='s-content'>{$row['s_add']}</p>";
+        echo "<p class='s-content'><b>景點資訊</b></p>";
+        echo "<p class='s-content'>{$row['s_info']}</p>";
+        echo "<p class='s-content'><b>在此取景作品</b></p>";
+
+        
+        if (!empty($row['movie_names'])) {
+            echo "<a href='movie-detail.php?d_id=" . $row['d_id'] . "'><p class='s-content drama'><b>{$row['movie_names']}</b></p></a>";
+        }
+
+        
+        if (!empty($row['drama_names'])) {
+            echo "<a href='drama-detail.php?m_id=" . $row['m_id'] . "'><p class='s-content drama'><b>{$row['drama_names']}</b></p></a>";
+        }
+
+        echo "</div>";
+        echo "</div>";
+        echo "<div style='position: relative;'><a style='font-size:20px;position:absolute;bottom:12px' href='spot-info.php?s_id=" . $row['s_id'] . "'><i class='fa-solid fa-circle-info' style='color: #1d50a1;'>&nbsp;&nbsp;詳細資訊</i></a></div>";
+        echo "</div>";
     }
 
     mysqli_free_result($result);
+} else {
+    echo "Error executing query: " . mysqli_error($conn);
 }
+
 mysqli_close($conn);
 ?>
+
 
 
             <!-- <div class="col-lg-8 col-md-8 col-sm-6 section_title" style="display:grid; grid-template-columns: 1fr 5fr;">
