42d4b0b50d3084a2e9771e4ca70ce0955d3fd68e
wennychang <131533115+wennychang@users.noreply.github.com>
2024-05-06 23:56:31 +0800

    Add files via upload

--- filter.php
+++ filter.php
@@ -1,41 +1,41 @@
 <?php
-    include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
-    $conn = sql_open();
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-    $genre = $_GET['genre'] ?? '';
-    
-    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['genre'])) {
-        $genre = mysqli_real_escape_string($conn, $_GET['genre']);
-    
-        $sql = "SELECT 'drama' AS source, d.d_id AS id, d.d_name AS name, d.d_pic AS img
-                FROM drama d
-                INNER JOIN genred gd ON d.d_id = gd.d_id
-                INNER JOIN genre g ON gd.g_id = g.g_id
-                WHERE g.g_id = '$genre'";
-    
-        $sql .= " UNION
-                SELECT 'movie' AS source, m.m_id AS id, m.m_name AS name, m.m_pic AS img
-                FROM movie m
-                INNER JOIN genrem gm ON m.m_id = gm.m_id
-                INNER JOIN genre g ON gm.g_id = g.g_id
-                WHERE g.g_id = '$genre'";
-    
-        $result = mysqli_query($conn, $sql);
-    
-        $response = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            $response[] = $row;
-        }
-    
-        echo json_encode($response);
-    } else {
+include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+$conn = sql_open();
 
-        echo json_encode(array());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$genre = $_GET['genre'] ?? '';
+$type = $_GET['type'] ?? '';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['genre'])) {
+    $genre = mysqli_real_escape_string($conn, $_GET['genre']);
+    $type = mysqli_real_escape_string($conn, $_GET['type'] ?? '');
+
+    $sql = "SELECT 'drama' AS source, d.d_id AS id, d.d_name AS name, d.d_pic AS img
+            FROM drama d
+            INNER JOIN genred gd ON d.d_id = gd.d_id
+            INNER JOIN genre g ON gd.g_id = g.g_id
+            WHERE g.g_id = '$genre'";
+
+    if ($type !== '') {
+        $sql .= " AND 'drama' = '$type'";
     }
+
+    $sql .= " UNION
+              SELECT 'movie' AS source, m.m_id AS id, m.m_name AS name, m.m_pic AS img
+              FROM movie m
+              INNER JOIN genrem gm ON m.m_id = gm.m_id
+              INNER JOIN genre g ON gm.g_id = g.g_id
+              WHERE g.g_id = '$genre'";
+
+    if ($type !== '') {
+        $sql .= " AND 'movie' = '$type'";
+    }
+
+    $result = mysqli_query($conn, $sql);
+}
 ?>
     
 
