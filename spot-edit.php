ff6db26f41851cec20c26a2ae0165f27972d190b
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-08 00:37:43 +0800

    Update spot-edit.php

--- spot-edit.php
+++ spot-edit.php
@@ -52,67 +52,134 @@
         include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
         $conn = sql_open();
         $s_id = $_GET["s_id"];
-
+    
         $sql = "SELECT spot.*, cspot.c_id, country.c_name 
         FROM spot 
         INNER JOIN cspot ON spot.s_id = cspot.s_id 
         INNER JOIN country ON cspot.c_id = country.c_id 
-        WHERE spot.s_id = '$s_id';
-        ";
-
+        WHERE spot.s_id = '$s_id';";
+    
         $result = mysqli_query($conn,$sql);
         if ($row = mysqli_fetch_assoc($result)){
-        $existingData = [
-            's_name' => $row['s_name'],
-            's_add' => $row['s_add'],
-            's_info' => $row['s_info'],
-            'lat_lon' => $row['lat_lon'],
-            's_intro' => $row['s_intro'],
-            's_photo' => $row['s_photo'],
-            's_pic' => $row['s_pic'],
+            $existingData = [
+                's_name' => $row['s_name'],
+                's_add' => $row['s_add'],
+                's_info' => $row['s_info'],
+                'lat_lon' => $row['lat_lon'],
+                's_intro' => $row['s_intro'],
+                's_photo' => $row['s_photo'],
+                's_pic' => $row['s_pic'],
+                'c_id' => $row['c_id']
+            ];
             
-            'c_id' => $row['c_id']
-
-        ];
-        }else{
-        echo "找不到對應資料";
-        exit();
-    }
+            // 查詢 spotm 表是否有符合的資料
+            $sql_spotm = "SELECT m_id FROM spotm WHERE s_id = '$s_id'";
+            $result_spotm = mysqli_query($conn, $sql_spotm);
+            if (mysqli_num_rows($result_spotm) > 0) {
+                $row_spotm = mysqli_fetch_assoc($result_spotm);
+                $m_id = $row_spotm['m_id'];
+                $sql_production_name = "SELECT m_name AS production_name FROM movie WHERE m_id = '$m_id'";
+            } else {
+                // 如果 spotm 表中沒有符合的資料，則查詢 spotd 表
+                $sql_spotd = "SELECT d_id FROM spotd WHERE s_id = '$s_id'";
+                $result_spotd = mysqli_query($conn, $sql_spotd);
+                if (mysqli_num_rows($result_spotd) > 0) {
+                    $row_spotd = mysqli_fetch_assoc($result_spotd);
+                    $d_id = $row_spotd['d_id'];
+                    $sql_production_name = "SELECT d_name AS production_name FROM drama WHERE d_id = '$d_id'";
+                }
+            }
+    
+            // 執行 SQL 查詢以獲取 production_name
+            if (isset($sql_production_name)) {
+                $result_production_name = mysqli_query($conn, $sql_production_name);
+                if ($row_production_name = mysqli_fetch_assoc($result_production_name)) {
+                    $existingData['production_name'] = $row_production_name['production_name'];
+                }
+            }
+    
+        } else {
+            echo "找不到對應資料";
+            exit();
+        }
         mysqli_close($conn);
     }
+    
     ?> 
 
     <!--修改PHP-->
     <?php
-    if (isset($_POST["Update"])) {
-        include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
-        $conn = sql_open();
-        $s_id = $_POST["s_id"];
-        $s_name = $_POST["s_name"];
-        $s_add = $_POST["s_add"];
-        $lat_lon = $_POST["lat_lon"];
-        $s_intro = $_POST["s_intro"];
-        $s_info = $_POST["s_info"];
-        $s_photo = $_POST["s_photo"];
-        $s_pic = $_POST["s_pic"];
-        $c_id = $_POST["c_id"];
+if (isset($_POST["Update"])) {
+    include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+    $conn = sql_open();
+    $s_id = $_POST["s_id"];
+    $s_name = $_POST["s_name"];
+    $s_add = $_POST["s_add"];
+    $lat_lon = $_POST["lat_lon"];
+    $s_intro = $_POST["s_intro"];
+    $s_info = $_POST["s_info"];
+    $s_photo = $_POST["s_photo"];
+    $s_pic = $_POST["s_pic"];
+    $c_id = $_POST["c_id"];
+    $production_name = $_POST["production_name"];
+    
+    // 查詢電影表中是否有符合的資料
+    $sql = "SELECT m_id FROM movie WHERE m_name = '$production_name'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $production_id = $row['m_id'];
         
-
+        // 更新 spot 表
         $sql = "UPDATE spot SET s_name = '$s_name', s_add = '$s_add', s_intro = '$s_intro', s_info = '$s_info', s_photo = '$s_photo', s_pic = '$s_pic', lat_lon = '$lat_lon' WHERE s_id='$s_id' ";
         mysqli_query($conn, $sql);
+        
+        // 更新 cspot 表
         $sql = "UPDATE cspot SET c_id = '$c_id' WHERE s_id='$s_id'";
-
+        mysqli_query($conn, $sql);
+        
+        // 更新 spotm 表
+        $sql = "UPDATE spotm SET m_id = '$production_id' WHERE s_id = '$s_id'";
         if (mysqli_query($conn, $sql)) {
             mysqli_close($conn);
             echo '<script>window.location.href = "spot-manage.php";</script>';
-            exit();
-        } else { 
+            exit(); 
+        } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
-
-        
+    } else {
+        // 查詢劇集表中是否有符合的資料
+        $sql = "SELECT d_id FROM drama WHERE d_name = '$production_name'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $production_id = $row['d_id'];
+            
+            // 更新 spot 表
+            $sql = "UPDATE spot SET s_name = '$s_name', s_add = '$s_add', s_intro = '$s_intro', s_info = '$s_info', s_photo = '$s_photo', s_pic = '$s_pic', lat_lon = '$lat_lon' WHERE s_id='$s_id' ";
+            mysqli_query($conn, $sql);
+            
+            // 更新 cspot 表
+            $sql = "UPDATE cspot SET c_id = '$c_id' WHERE s_id='$s_id'";
+            mysqli_query($conn, $sql);
+            
+            // 更新 spotd 表
+            $sql = "UPDATE spotd SET d_id = '$production_id' WHERE s_id = '$s_id'";
+            if (mysqli_query($conn, $sql)) {
+                mysqli_close($conn);
+                echo '<script>window.location.href = "spot-manage.php";</script>';
+                exit(); 
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        } else {
+            echo "找不到對應的劇集或電影資料";
+        }
     }
-    ?>
+}
+
+?>
+
 
     <!-- Product Section Begin -->
     <section class="product-page spad">
@@ -143,17 +210,28 @@
                         <input type="text" name="s_add" class="form-control" placeholder="請輸入地址" value="<?php echo $existingData['s_add'];?>">
 
                         <label for="inputEmail4" style="margin-top: 25px;">景點座標</label>
-                        <input type="text" class="form-control" placeholder="請輸入座標" value="<?php echo $existingData['lat_lon'];?>">
+                        <input type="text" name="lat_lon" class="form-control" placeholder="請輸入座標" value="<?php echo $existingData['lat_lon'];?>">
 
                         <label for="inputEmail4" style="margin-top: 25px;">景點資訊</label>
                         <input type="text" name="s_info" class="form-control" placeholder="請輸入營業時間、電話等資訊" value="<?php echo $existingData['s_info'];?>">
 
                         <label for="inputEmail4" style="margin-top: 25px;">在此取景作品</label><br>
-                        <select class="form-control select" id="exampleFormControlSelect1" >
-                        
-                            
-                            
+                        <select class="form-control select" id="exampleFormControlSelect1" name="production_name">
+                            <?php 
+                            include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+                            $conn = sql_open();
+                            $sql = "SELECT d_name AS production_name FROM drama UNION SELECT m_name AS production_name FROM movie";
+                            if ($result = mysqli_query($conn, $sql)) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $selected = ($row['production_name'] == $existingData['production_name']) ? 'selected' : ''; 
+                                    echo "<option value='" . $row['production_name'] . "' $selected>" . $row['production_name'] . "</option>";
+                                }
+                                mysqli_free_result($result);
+                            }
+                            mysqli_close($conn);
+                            ?>
                         </select>
+
                         
                     </div>
                     <div class="col">
@@ -204,49 +282,9 @@
     </section>
 <!-- Product Section End -->
 
-<!-- Footer Section Begin -->
-<footer class="footer">
-    <div class="page-up">
-        <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="footer__logo">
-                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="footer__nav">
-                    <ul>
-                        <li class="active"><a href="./index.html">Homepage</a></li>
-                        <li><a href="./categories.html">Categories</a></li>
-                        <li><a href="./blog.html">Our Blog</a></li>
-                        <li><a href="#">Contacts</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
 
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- Footer Section End -->
 
-<!-- Search model Begin -->
-<div class="search-model">
-    <div class="h-100 d-flex align-items-center justify-content-center">
-        <div class="search-close-switch"><i class="icon_close"></i></div>
-        <form class="search-model-form">
-            <input type="text" id="search-input" placeholder="Search here.....">
-        </form>
-    </div>
-</div>
-<!-- Search model end -->
+
 
 <!-- Js Plugins -->
 <script src="js/jquery-3.3.1.min.js"></script>
