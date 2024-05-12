415308afef2513c4f333fe24dac187f37d58d31e
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-12 21:21:33 +0800

    Add files via upload

---
+++ sd-del.php
@@ -0,0 +1,22 @@
+<?php 
+    if (isset($_GET["s_id"] ) && isset($_GET["sd_id"])) {
+        include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+        $conn = sql_open();
+
+        $s_id = $_GET["s_id"];
+        $sd_id = $_GET["sd_id"];
+
+
+        $sql = "DELETE FROM spotd WHERE sd_id = '$sd_id'";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "刪除成功";
+        } else {
+            echo "錯誤： " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+        header("Location:spot-edit.php?s_id=$s_id");
+        exit();
+    }
+?>
---
+++ sm-del.php
@@ -0,0 +1,22 @@
+<?php 
+    if (isset($_GET["s_id"] ) && isset($_GET["sm_id"])) {
+        include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+        $conn = sql_open();
+
+        $s_id = $_GET["s_id"];
+        $sm_id = $_GET["sm_id"];
+
+
+        $sql = "DELETE FROM spotm WHERE sm_id = '$sm_id'";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "刪除成功";
+        } else {
+            echo "錯誤： " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+        header("Location:spot-edit.php?s_id=$s_id");
+        exit();
+    }
+?>
