ee377bedb9d813ca970a64244044f713c63ac264
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-12 21:09:27 +0800

    Update spot-del.php

--- spot-del.php
+++ spot-del.php
@@ -7,15 +7,38 @@
 
 
         $sql = "DELETE FROM spot WHERE s_id = '$sid'";
+        mysqli_query($conn, $sql);
 
-        if (mysqli_query($conn, $sql)) {
-            echo "刪除成功";
-        } else {
-            echo "錯誤： " . $sql . "<br>" . mysqli_error($conn);
+        $sql = "DELETE FROM cspot WHERE s_id = '$sid'";
+        mysqli_query($conn, $sql);
+
+        $sql = "SELECT * FROM spotm WHERE s_id = '$s_id'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            
+            $delete_sql = "DELETE FROM spotm WHERE s_id = '$s_id'";
+            if (mysqli_query($conn, $delete_sql)) {
+                echo "成功刪除 spotm 上的相應記錄";
+            } else {
+                echo "刪除 spotm 上的記錄時出現錯誤：" . mysqli_error($conn);}
+
+        }
+
+        $sql = "SELECT * FROM spotd WHERE s_id = '$s_id'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            
+            $delete_sql = "DELETE FROM spotd WHERE s_id = '$s_id'";
+            if (mysqli_query($conn, $delete_sql)) {
+                echo "成功刪除 spotd 上的相應記錄";
+            } else {
+                echo "刪除 spotd 上的記錄時出現錯誤：" . mysqli_error($conn);}
+
         }
 
         mysqli_close($conn);
         header("Location:spot-manage.php");
         exit();
-    }
+    
+}
 ?>
