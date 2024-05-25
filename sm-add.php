8e75efad0e29a1c5370b512a29dfe41691aec78a
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-25 14:39:43 +0800

    Update sm-add.php

--- sm-add.php
+++ sm-add.php
@@ -10,12 +10,10 @@ if (isset($_GET["s_id"] ) && isset( $_GET["m_id"])) {
     
     if (mysqli_query($conn, $sql)) {
         mysqli_close($conn);
-        header("Location:spot-edit.php?s_id=$s_id");
+        header("Location:spot-edit.php?s_id=$s_id#b");
         exit();
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-}
+} 
 ?>
-
-
