7bdb2b27b22f310967c66ebd7574b92a125636ff
Mandy1122-lab <130623600+Mandy1122-lab@users.noreply.github.com>
2024-05-26 14:16:03 +0800

    Update spot-add.php

--- spot-add.php
+++ spot-add.php
@@ -28,17 +28,14 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
     <script src="https://kit.fontawesome.com/937e93c93c.js" crossorigin="anonymous"></script>
     <style>
-        .nav-item a{
-            color:black !important;
-            background-color:#EEEEEE;
+        .nav-item a {
+            color: black !important;
+            background-color: #EEEEEE;
         }
-        .tab-content{
-            margin:30px auto 30px auto;
+        .tab-content {
+            margin: 30px auto 30px auto;
         }
     </style>
-    
-
-    
 </head>
 
 <body class="font_set">
@@ -52,50 +49,51 @@
     include 'header-mng.html';
     echo "<br>";
     echo "<a href='spot-manage.php' style='margin-left:150px'><i class='fa-solid fa-arrow-left fa-xl' style='color: #1d50a1;'></i></a>";
-
     ?>
     <!-- Header End -->
+
     <!--新增景點PHP-->
     <?php 
-if (isset($_POST["Insert"])) {
-    include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
-    $conn = sql_open();
-    if (!$conn) {
-        die("資料庫連線失敗: " . mysqli_connect_error());
-    }
-
-    $s_name = $_POST["s_name"];
-    $s_add = $_POST["s_add"];
-    $s_info = $_POST["s_info"];
-    $s_intro = $_POST["s_intro"];
-    $s_photo = $_POST["s_photo"];
-    $s_pic = $_POST["s_pic"]; 
-    $lat_lon = $_POST["lat_lon"];
-    $c_id = $_POST["c_id"];
-    
-
-    $sql = "INSERT INTO spot (s_name, s_add, s_info, s_intro, s_photo, s_pic, lat_lon) VALUES ('$s_name', '$s_add', '$s_info', '$s_intro', '$s_photo', '$s_pic', '$lat_lon')";
-        mysqli_query($conn, $sql);
-
-        $s_id = mysqli_insert_id($conn);
-
-        $sql = "INSERT INTO cspot (c_id, s_id) VALUES ('$c_id', '$s_id')";
-
-        if (mysqli_query($conn, $sql)) {
-            mysqli_close($conn);
-            header("Location: spot-edit.php?s_id=$s_id"); 
-            exit(); 
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    if (isset($_POST["Insert"])) {
+        include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
+        $conn = sql_open();
+        if (!$conn) {
+            die("資料庫連線失敗: " . mysqli_connect_error());
         }
+
+        $s_name = mysqli_real_escape_string($conn, $_POST["s_name"]);
+        $s_add = mysqli_real_escape_string($conn, $_POST["s_add"]);
+        $s_info = mysqli_real_escape_string($conn, $_POST["s_info"]);
+        $s_intro = mysqli_real_escape_string($conn, $_POST["s_intro"]);
+        $s_photo = mysqli_real_escape_string($conn, $_POST["s_photo"]);
+        $s_pic = mysqli_real_escape_string($conn, $_POST["s_pic"]); 
+        $lat_lon = mysqli_real_escape_string($conn, $_POST["lat_lon"]);
+        $c_id = intval($_POST["c_id"]);
+        $frame = mysqli_real_escape_string($conn, $_POST["frame"]);
+        
+        $sql = "INSERT INTO spot (s_name, s_add, s_info, s_intro, s_photo, s_pic, lat_lon, frame) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "ssssssss", $s_name, $s_add, $s_info, $s_intro, $s_photo, $s_pic, $lat_lon, $frame);
+        if (mysqli_stmt_execute($stmt)) {
+            $s_id = mysqli_insert_id($conn);
+
+            $sql = "INSERT INTO cspot (c_id, s_id) VALUES (?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "ii", $c_id, $s_id);
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_close($conn);
+                header("Location: spot-edit.php?s_id=$s_id");
+                exit();
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
     } 
-    
-
-
-?>
-
-
-
+    ?>
 
     <!-- Product Section Begin -->
     <section class="product-page spad">
@@ -120,18 +118,9 @@ if (isset($_POST["Insert"])) {
                         <label for="inputEmail4">景點名稱</label>
                         <input type="text" name="s_name" class="form-control" placeholder="請輸入名稱">
 
-                        <label for="inputEmail4" style="margin-top: 25px;">地址</label>
-                        <input type="text" name="s_add" class="form-control" placeholder="請輸入地址">
-                        <!--step2-->
-                        <label for="inputEmail4" style="margin-top: 25px;">景點座標</label>
-                        <input type="text" name="lat_lon" class="form-control" placeholder="請輸入座標">
-
-                        <label for="inputEmail4" style="margin-top: 25px;">景點資訊</label>
-                        <input type="text" name="s_info" class="form-control" placeholder="請輸入營業時間、電話等資訊">
-                        <!--step2-->
                         <div style="display:grid;grid-template-rows:1fr 2fr;margin-top:25px">
-                        <label for="inputEmail4" >國家</label>
-                        <select class="form-control select"  id="country" name="c_id" >
+                        <label for="inputEmail4">國家</label>
+                        <select class="form-control select" id="country" name="c_id">
                         <?php 
                             include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/lib_mysql.php";
                             $conn = sql_open();
@@ -139,19 +128,28 @@ if (isset($_POST["Insert"])) {
                             if ($result = mysqli_query($conn, $sql)) {
                                 while ($row = mysqli_fetch_assoc($result)) {
                                     echo "<option value='" . $row['c_id'] . "'>" . $row['c_name'] . "</option>";
-                                    
                                 }
                                 mysqli_free_result($result);
                             }
                             mysqli_close($conn);
                             ?>
                         </select></div>
-                        
-                        
+
+                        <label for="inputEmail4">地址</label>
+                        <input type="text" name="s_add" class="form-control" placeholder="請輸入地址">
+
+                        <label for="inputEmail4" style="margin-top: 25px;">景點座標</label>
+                        <input type="text" name="lat_lon" class="form-control" placeholder="請輸入座標">
+
+                        <label for="inputEmail4" style="margin-top: 25px;">嵌入 Google 地圖</label>
+                        <input type="text" name="frame" class="form-control" placeholder="請輸入 Google 地圖 HTML">
 
                     </div> 
                     <div class="col"> 
-                        <label for="inputEmail4">景點簡介</label>
+                        <label for="inputEmail4">景點資訊</label>
+                        <input type="text" name="s_info" class="form-control" placeholder="請輸入營業時間、電話等資訊">
+
+                        <label for="inputEmail4" style="margin-top: 25px;">景點簡介</label>
                         <textarea class="form-control" name="s_intro" placeholder="請輸入內容" rows="5"></textarea>
 
                         <label for="inputEmail4" style="margin-top: 25px;">相關劇照</label>
@@ -161,7 +159,7 @@ if (isset($_POST["Insert"])) {
                         <input type="text" name="s_pic" class="form-control" placeholder="請輸入位置">
                         
                         <nobr>
-                        <button type="reset" onclick="window.location.href='spot-manage.php'" class="btn btn-outline-primary cancel" >取消</button>
+                        <button type="reset" onclick="window.location.href='spot-manage.php'" class="btn btn-outline-primary cancel">取消</button>
                         <button type="submit" name="Insert" class="btn btn-outline-primary save">儲存</button>
                         </nobr>
 
@@ -170,28 +168,20 @@ if (isset($_POST["Insert"])) {
                     </div>
                 </form>
                 </div>
-    
-                
             </div>
-            
         </div>
     </section>
-<!-- Product Section End -->
+    <!-- Product Section End -->
 
-
-
-
-<!-- Js Plugins -->
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/player.js"></script>
-<script src="js/jquery.nice-select.min.js"></script>
-<script src="js/mixitup.min.js"></script>
-<script src="js/jquery.slicknav.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/main.js"></script>
-
-
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/player.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
